2b90d39bfdaabd02bd5aba9248bffb5f705a6bb6
TRomesh <tharakaromesh@gmail.com>
2023-06-06 20:55:49 +0200

    add detect-inapp package

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import useDetectInApp from "./hooks/useDetectInApp";
 import { detectInAppBrowser } from "./util";
 
+import InApp from "detect-inapp";
+
 function App() {
   const userAgent = navigator.userAgent;
   const { isInApp, isMobile, isDesktop, browser } = useDetectInApp(userAgent);
+  const inapp = new InApp(
+    navigator.userAgent || navigator.vendor || window.opera
+  );
 
   return (
     <>
@@ -12,6 +17,12 @@ function App() {
       <div>isDesktop : {isDesktop ? "true" : "false"}</div>
       <div>browser : {browser}</div>
       <div>detectInAppBrowser: {detectInAppBrowser(navigator.userAgent)}</div>
+      <br />
+      <div>isInApp : {inapp.isInApp ? "true" : "false"}</div>
+      <div>isMobile : {inapp.isMobile ? "true" : "false"}</div>
+      <div>isDesktop : {inapp.isDesktop ? "true" : "false"}</div>
+      <div>browser : {inapp.browser}</div>
+      <div>ua: {inapp.ua}</div>
     </>
   );
 }
